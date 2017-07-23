6dee79f4557495959ea5105babe9eef8fc4e4218
ysuyeon <yysuyeon@gmail.com>
2017-07-24 07:59:03 +0900

    Add one more :)

--- jungduck92/src/service/FactoryService.java
+++ jungduck92/src/service/FactoryService.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
